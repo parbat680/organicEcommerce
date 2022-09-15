9f70b337d021353fa9bff72345c5eb8b34d68717
prajpurohit680 <prajpurohit680@gmail.com>
2022-09-15 23:27:50 +0530

    added routes

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express= require('express')
 const mongoose= require('mongoose')
 const app= express()
 require('dotenv').config()
-
+const auth_routes= require('./routes/Authcontroller')
+const category_routes= require('./routes/category')
+const product_routes= require('./routes/product')
+const order_routes= require('./routes/orders')
 
 
 app.use(express.json())
@@ -20,6 +23,14 @@ database.once('connected',(connected)=> {
     console.log('Database connected...')
 })
 
+app.use('/user',auth_routes);
+app.use('/category',category_routes)
+app.use('/product',product_routes)
+app.use('/order',order_routes)
+
+app.get('/',(req,res)=> {
+    console.log('Welcome to Organic Ecommerce')
+})
 
 const port = process.env.PORT || 5000;
 
